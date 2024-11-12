0b4adb76dfa95a6fa7dad4232ce0d61b2e3c3dc1
Mark Parker <marksadpepe@gmail.com>
2024-11-12 13:19:15 +0200

    fixed bug with env variables

--- src/db/knexfile.ts
+++ src/db/knexfile.ts
@@ -1,5 +1,9 @@
 import {join} from "path";
+import {config} from "dotenv";
 import {getDbConfig, DBConfigType} from "../configs";
+
+const envPath = join(__dirname, "..", "..", ".env");
+config({ path: envPath })
 
 const databaseConfig: DBConfigType = getDbConfig();
 const migrationsDir = join(__dirname, "migrations");
