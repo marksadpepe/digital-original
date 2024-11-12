fb0a5591d87941f741694c9d976b86cb05dbb96a
Mark Parker <marksadpepe@gmail.com>
2024-11-12 13:43:46 +0200

    minor changes

--- src/db/migrations/20241112003547_artists.ts
+++ src/db/migrations/20241112003547_artists.ts
@@ -10,31 +10,32 @@ exports.up = function(knex: any) {
   .then(() => {
     return knex("Artists").insert([
       {
-        name_original: 'Pablo Picasso',
+        name_original: "Pablo Picasso",
         name_english: null,
-        photo_id: '550e8400-e29b-41d4-a716-446655440003',
+        photo_id: "550e8400-e29b-41d4-a716-446655440003",
         gallery_id: 1
       },
       {
-        name_original: 'Vincent van Gogh',
+        name_original: "Vincent van Gogh",
         name_english: null,
-        photo_id: '550e8400-e29b-41d4-a716-446655440004',
+        photo_id: "550e8400-e29b-41d4-a716-446655440004",
         gallery_id: 1
       },
       {
-        name_original: 'Юрій Болса',
-        name_english: 'Yurii Bolsa',
+        name_original: "Юрій Болса",
+        name_english: "Yurii Bolsa",
         photo_id: null,
         gallery_id: 2
       },
       {
-        name_original: 'Leonardo da Vinci',
+        name_original: "Leonardo da Vinci",
         name_english: null,
         photo_id: null,
         gallery_id: 2
       },
       {
-        name_original: 'Mark Parker',
+        // new user just for demonstration
+        name_original: "Mark Parker",
         name_english: null,
         photo_id: null,
         gallery_id: 1
